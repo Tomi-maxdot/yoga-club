d87a4790c5c4291c6a9c85052f253ecd0bbe7e9c
Tomi <olaoluwaakomolafe08@gmail.com>
2026-09-07 21:01:27 +0100

    first commit

--- main.js
+++ main.js
@@ -7,46 +7,47 @@ menuBtn.addEventListener("click", () => {
 
     const isOpen = navLinks.classList.contains("open");
     menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-4-line");
+
 });
 
 navLinks.addEventListener("click", () => {
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "ri-menu-4-line");
-})
+});
 
 const scrollRevealOption = {
     distance: "50px",
     origin: "bottom",
     duration: 1000,
-}
+};
+
 ScrollReveal().reveal(".header_image img", {
     ...scrollRevealOption,
     origin: "right",
-})
+});
 
 ScrollReveal().reveal(".header_content h2", {
     ...scrollRevealOption,
     delay: 500,
-})
+});
 
 ScrollReveal().reveal(".header_content h1", {
     ...scrollRevealOption,
     delay: 1000,
-})
+});
 
 ScrollReveal().reveal(".header_content p", {
     ...scrollRevealOption,
     delay: 1500,
-})
+});
 
 ScrollReveal().reveal(".header_btn", {
     ...scrollRevealOption,
     delay: 2000,
-})
+});
 
 ScrollReveal().reveal(".header_socials li", {
     ...scrollRevealOption,
     delay: 2500,
     interval: 500,
-})
-
+});
